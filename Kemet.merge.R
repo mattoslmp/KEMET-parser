4ca820a3c853e28cb419f319eaf5184f653075f5
Leandro de Mattos <mattoslmp@gmail.com>
2022-09-19 15:33:58 -0300

    Update Kemet.merge.R

--- Kemet.merge.R
+++ Kemet.merge.R
@@ -1,41 +1,44 @@
 
 library(readr)
-#Autor: Leandro de Mattos Pereira 12/02/2018
-###Leandro de Mattos Pereira
-###Gisele Nunes
-#### Fun??es usadas: Phyloseq, Melt and ggplot, biomformat,dplyr"
+#Authors: Leandro de Mattos Pereira 12/02/2018
+#Leandro de Mattos Pereira
+#Gisele Nunes
+
 rm(list=ls())
+
 library (purrr)
 library(readr)
 library(ggpubr)
 library(stringr)
-############################ IMPORTACAO DE DADOS DO QIIME2
+############################ To specify KEMET results directory:
+
 setwd ("D:/ITV/KEMET_resultados/reports_tsv_KASS")
 
 ###path: To specify directory contain KEMET results:
-data_join <-list.files(path="D:/ITV/KEMET_resultados/reports_tsv_KASS/", pattern="*.tsv", full.names=TRUE) %>% 
-lapply(read_tsv) %>%  
-reduce(full_join, by = "Module_id")  %>% unique()                     
-data_join               
 
-modules_id <- data_join$Module_id # colname: module_id
-modules_names <- data_join$Module_name.x # colname: module_name
+data_join <-list.files(path="D:/ITV/KEMET_resultados/reports_tsv_KASS/", pattern="*.tsv", full.names=TRUE) 
+%>% lapply(read_tsv) %>%  
+reduce(full_join, by = "Module_id")  %>% unique()                     
+
+# colname: module_id
+modules_id <- data_join$Module_id 
+# colname: module_name
+modules_names <- data_join$Module_name.x 
 
 df <- data_join %>%
   select(matches("(Completeness)"))
 
-
-#My Filenames pattern of KEMET results: reportKMC_Ga0541012_bin.tsv
-
 myfilenames <-list.files(path="D:/ITV/KEMET_resultados/reports_tsv_KASS/", pattern="*.tsv", full.names=TRUE) 
+
+#My Filenames pattern of KEMET results: reportKMC_Ga0541012_bin.tsv, get all names of samples:
+
 name_files <- sapply(strsplit(myfilenames, split='reportKMC_', fixed=TRUE), function(x) (x[2]))
 name_files <- str_remove(name_files, pattern = ".tsv")
 
 df2 <- data.frame(modules_id, modules_names, df)
+
+### To create a new data.frame with results and name of samples by collums
 colnames(df2) <- c ("Module_id", "Completeness", name_files)
-colnames(df2) <- c ("Module_id", "Completeness","AM.P1.D","AM.P1.R","AM.P2.D","AM.P2.R","TIA.P1.D","TIA.P1.R",
-  "TIA.P2.D","TIA.P2.R","TI.P1.D","TI.P1.R","TI.P2.D","TI.P2.R",
-  "TI.P3.D","TI.P3.R","TI.P4.D","TI.P4.R","VI.P1.D","VI.P1.R",
-  "VI.P2.D","VI.P2.R")
+# Export data.frame with all KEMET results into a only table
 write.table (df2, "Res_KEMET.tsv",row.names=FALSE, sep="\t")
 
